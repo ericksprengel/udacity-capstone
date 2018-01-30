c3ddfab33877ecc0fc6472ba7e47907932f5d2e1
Erick Massa Sprengel <erick.sprengel@gmail.com>
2018-01-30 07:44:09 -0200

    add authentication - step 32

--- app/src/main/java/br/com/ericksprengel/marmitop/ui/main/MainActivity.java
+++ app/src/main/java/br/com/ericksprengel/marmitop/ui/main/MainActivity.java
@@ -1,21 +1,33 @@
 package br.com.ericksprengel.marmitop.ui.main;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 import br.com.ericksprengel.mamitop.data.MtopMenuItem;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+
+import br.com.ericksprengel.marmitop.BuildConfig;
 import br.com.ericksprengel.marmitop.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -41,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_menu:
                     MtopMenuItem menuItem = new MtopMenuItem();
                     menuItem.setName("Feijoada");
-                    menuItem.setDescription("arroz, feijão e fritas." + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+                    menuItem.setDescription("arroz, feijão e fritas."  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
 
                     Toast.makeText(MainActivity.this, menuItem.getDescription(), Toast.LENGTH_SHORT).show();
                     mMenus.child(new SimpleDateFormat("yyyy-MM-dd").format(new Date())).push().setValue(menuItem);
@@ -67,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
         // Database init
         mDatabase = FirebaseDatabase.getInstance();
         mMenus = mDatabase.getReference("menus");
+
 
         mBottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         mPagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
@@ -136,6 +149,103 @@ public class MainActivity extends AppCompatActivity {
             public void onPageScrollStateChanged(int state) {
             }
         });
+
+        authenticate();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    private void loadMenu() {
+        Toast.makeText(this, "TODO: load menu", Toast.LENGTH_LONG).show();
+    }
+
+
+
+
+    // FIREBASE AUTH
+
+    private static final int RC_SIGN_IN = 123;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    public void authenticate() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    loadMenu();
+                    Toast.makeText(MainActivity.this, "You're now signed in. Welcome to FriendlyChat.", Toast.LENGTH_SHORT).show();
+                } else {
+                    // User is signed out
+                    AuthUI.IdpConfig idpConfigPhone = new AuthUI.IdpConfig.PhoneBuilder()
+                            .setDefaultCountryIso("br")
+                            .build();
+
+                    MainActivity.this.startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            idpConfigPhone,
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .setIsSmartLockEnabled(!BuildConfig.DEBUG, true)
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //TODO: super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                this.startActivity(SignedInActivity.createIntent(this, response));
+                this.finish();
+                return;
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(this, "TODO(string): Sign in cancelled", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, "TODO(string): No internet connection", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(this, "TODO(string): Unknown error", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+
+            Toast.makeText(this, "TODO(string): unknown_sign_in_response", Toast.LENGTH_LONG).show();
+        }
+    }
 }
